1e83cbe96f37b8b0bb10e099477321bc925c54df
bonface <bonface@swiftlabs.ai>
2021-09-17 09:02:38 +0300

    add endpoint to confirm user for password reset

--- routes/auth.js
+++ routes/auth.js
@@ -41,4 +41,17 @@ router.post("/login", async(req, res) => {
     }
 })
 
+//CONFIRM USER FOR PASSWORD CHANGE
+router.post("/confirmuser", async(req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(404).json("user not found");
+        
+        res.status(200).json(user);
+
+    } catch(err) {
+        res.status(500).json(err);
+    }  
+})
+
 module.exports = router;
